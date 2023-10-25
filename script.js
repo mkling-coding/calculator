048d299fad269adc303fd6a651c800e88fd2e83c
Mark Klingelhoefer <mkling5577@gmail.com>
2023-10-25 18:19:52 -0400

    Add basic operator functions
    Add functions that add, subtract, multiply, & divide two numbers

---
+++ script.js
@@ -0,0 +1,16 @@
+// Basic math operators
+const add = (num1, num2) => {
+    return num1 + num2;
+};
+
+const subtract = (num1, num2) => {
+    return num1 - num2;
+};
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+const divide = (num1, num2) => {
+    return num1/num2;
+}
