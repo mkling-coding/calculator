24a43dde17fd5b6e05744e64a4c40a99acded44e
Mark Klingelhoefer <mkling5577@gmail.com>
2023-10-25 18:25:40 -0400

    Add parts of operation and add operate function
    Operate function determines what the operator is and then runs the
    correct function (add, subtract, divide, or multiply) for the two
    numbers

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ const multiply = (num1, num2) => {
 const divide = (num1, num2) => {
     return num1/num2;
 }
+
+// Variables for each part of a calculator operation
+let firstNumber;
+let secondNumber;
+let operator;
+
+// Takes an operator and 2 numbers and then calls one of the above functions on the numbers
+const operate = (num1, num2, operator) => {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2)
+    } else {
+        return "Error: invalid operator"
+    }
+};
